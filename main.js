0f961a68cee7d7f94fb949fb7016e5f9844250a5
Vicky Chijwani <vickychijwani@gmail.com>
2013-10-01 16:05:55 +0530

    cleaner UI

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-var client = new Dropbox.Client({key:'75g7txpckgllo8y'});
-client.authDriver(new Dropbox.AuthDriver.Redirect({rememberUser : false}));
-if (!client.isAuthenticated()){
-  client.authenticate(function(error, client){
-    if(error){
+var client = new Dropbox.Client({ 'key': '75g7txpckgllo8y'});
+
+client.authDriver(new Dropbox.AuthDriver.Redirect({ 'rememberUser': true }));
+
+if (! client.isAuthenticated()) {
+  client.authenticate(function (error, client) {
+    if (error) {
       console.log(error);
     }
     console.log(client);
@@ -11,32 +13,35 @@ if (!client.isAuthenticated()){
 
 listDirInHTML(client, "/");
 $('#parentDirectory').hide();
-$('#newFile').click(function(){
+
+$('#newFile').click(function () {
   $('#newFileName').focus();
 });
-$('#newDir').click(function(){
+
+$('#newDir').click(function () {
   $.msgBox({
-    type:"prompt", 
-    title:"Directory name", 
-    inputs:[{header:"Name", type:"text"}], 
-    buttons:[{value:"OK"}, {value:"Cancel"}], 
-    success: function (result, values) {
-      if (result == "OK"){
-        if(values[0].value == null || values[0].value == ""){
+    "type": "prompt",
+    "title": "Directory name",
+    "inputs": [{header:"Name", type:"text"}],
+    "buttons": [{value:"OK"}, {value:"Cancel"}],
+    "success": function (result, values) {
+      if (result === "OK") {
+        if (values[0].value === null || values[0].value === "") {
           displayError("Need a directory name");
         }
-        else{
+        else {
           createDirectory(client, pathString(), values[0].value);
         }
       }
     }
   });
 });
-$('#createFile').click(function(){
+
+$('#createFile').click(function () {
   createFile(client, pathString(), $('#newFileName').val(), $('#fileContent').val(), $('#key').val());
 });
 
-$('#reloadDir').click(function(){
+$('#reloadDir').click(function () {
   console.log("Reloading...");
   dirResults[pathString()] = null;
   listDirInHTML(client, pathString());
